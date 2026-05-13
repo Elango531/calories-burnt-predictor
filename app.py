6fe4c76acbdc64d64497704ce996d3b244eb555d
Elango531 <elango531537@gmail.com>
2026-05-13 10:55:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import joblib
 import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+def train_model():
+    df = pd.read_csv("calories.csv")
+    le = LabelEncoder
+    df["Gender"] = le.fit_transform(df["Gender"]
+     df = df.drop(columns=['User_ID'])
+    X = df.drop(columns=['Calories'])
+    y = df['Calories']
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    return model
 
 model = joblib.load("calories_model.pkl")
 
