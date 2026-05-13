a547c1fdd55f5d082370d375bcceee419fabf40a
Elango531 <elango531537@gmail.com>
2026-05-13 10:57:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 def train_model():
     df = pd.read_csv("calories.csv")
     le = LabelEncoder
-    df["Gender"] = le.fit_transform(df["Gender"]
+    df["Gender"] = le.fit_transform(df["Gender"])
      df = df.drop(columns=['User_ID'])
     X = df.drop(columns=['Calories'])
     y = df['Calories']
